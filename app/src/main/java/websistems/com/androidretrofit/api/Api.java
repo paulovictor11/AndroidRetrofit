121a1b383347d8a1916b16bac8f76b5889c1460b
paulovictor11 <pauloalmeidajp652@gmail.com>
2018-07-22 21:23:04 -0300

    Versão 0.8

--- app/src/main/java/websistems/com/androidretrofit/api/Api.java
+++ app/src/main/java/websistems/com/androidretrofit/api/Api.java
@@ -5,6 +5,8 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 import websistems.com.androidretrofit.models.DefaultResponse;
 import websistems.com.androidretrofit.models.LoginResponse;
 import websistems.com.androidretrofit.models.UsersResponse;
@@ -29,4 +31,21 @@ public interface Api {
 
     @GET("allusers")
     Call<UsersResponse> getUsers();
+
+    @FormUrlEncoded
+    @PUT("updateuser/{id}")
+    Call<LoginResponse> updateUser(
+            @Path("id") int id,
+            @Field("email") String email,
+            @Field("name") String name,
+            @Field("school") String school
+    );
+
+    @FormUrlEncoded
+    @PUT("updatepassword")
+    Call<DefaultResponse> updatePassword(
+            @Field("currentPassword") String currentPassword,
+            @Field("newPassword") String newPassword,
+            @Field("email") String email
+    );
 }
--- app/src/main/java/websistems/com/androidretrofit/fragments/SettingsFragment.java
+++ app/src/main/java/websistems/com/androidretrofit/fragments/SettingsFragment.java
@@ -4,17 +4,128 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import websistems.com.androidretrofit.R;
+import websistems.com.androidretrofit.api.RetrofitClient;
+import websistems.com.androidretrofit.models.LoginResponse;
+import websistems.com.androidretrofit.models.User;
+import websistems.com.androidretrofit.storage.SharedPrefManager;
 
-public class SettingsFragment extends Fragment{
+public class SettingsFragment extends Fragment implements View.OnClickListener{
+
+    private EditText edtEmail, edtName, edtSchool;
+    private EditText edtCurrentePassword, edtNewPassword;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.settings_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        edtEmail = view.findViewById(R.id.edtUpdateEmail);
+        edtName = view.findViewById(R.id.edtUpdateName);
+        edtSchool = view.findViewById(R.id.edtUpdateSchool);
+        edtCurrentePassword = view.findViewById(R.id.edtCurrentPasword);
+        edtNewPassword = view.findViewById(R.id.edtNewPasword);
+
+        view.findViewById(R.id.btnSave).setOnClickListener(this);
+        view.findViewById(R.id.btnChange).setOnClickListener(this);
+        view.findViewById(R.id.btnLogout).setOnClickListener(this);
+        view.findViewById(R.id.btnDelete).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnSave:
+                updateProfile();
+                break;
+
+            case R.id.btnChange:
+                updatePassword();
+                break;
+
+            case R.id.btnLogout:
+                break;
+
+            case R.id.btnDelete:
+                break;
+        }
+    }
+
+    private void updateProfile() {
+        String email = edtEmail.getText().toString().trim();
+        String name = edtName.getText().toString().trim();
+        String school = edtSchool.getText().toString().trim();
+
+        if (email.isEmpty()){
+            edtEmail.setError("Email is required");
+            edtEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            edtEmail.setError("Enter a valid email");
+            edtEmail.requestFocus();
+            return;
+        }
+
+        if (name.isEmpty()){
+            edtName.setError("Name required");
+            edtName.requestFocus();
+            return;
+        }
+
+        if (school.isEmpty()){
+            edtSchool.setError("School required");
+            edtSchool.requestFocus();
+            return;
+        }
+
+        User user = SharedPrefManager.getInstance(getActivity()).getUser();
+
+        Call<LoginResponse> call = RetrofitClient
+                .getInstance()
+                .getApi()
+                .updateUser(
+                        user.getId(),
+                        email,
+                        name,
+                        school
+                );
+        call.enqueue(new Callback<LoginResponse>() {
+            @Override
+            public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                Toast.makeText(getActivity(), response.body().getMessage(), Toast.LENGTH_LONG).show();
+
+                if (!response.body().isError()){
+                    SharedPrefManager.getInstance(getActivity()).saveUser(response.body().getUser());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LoginResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void updatePassword() {
+        User user = SharedPrefManager.getInstance(getActivity()).getUser();
+
+
     }
 }
